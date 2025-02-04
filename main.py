a04c79a3682e375cc25bc93286d37c809bff88c9
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2025-02-04 13:00:26 -0500

    Added lunar environment, added visualization for reward decomposition

---
+++ main.py
@@ -0,0 +1,27 @@
+import gymnasium as gym
+from envs.lunar_lander import LunarLander, demo_heuristic_lander
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+env = LunarLander()
+
+total_reward, rewards = demo_heuristic_lander(env)
+# rewards = np
+print(total_reward)
+rewards = np.array(rewards)
+
+df = pd.DataFrame(rewards, columns = ["distance", "Speed", "Tilt", "Leg 1", "Leg 2", "main engine", "side engine", "success"])
+
+
+plt.figure(figsize = (10, 5))
+df.iloc[:-2].plot()
+plt.savefig("RewardDecom.png")
+
+plt.figure(figsize = (10, 5))
+sns.barplot(df.sum())
+plt.savefig("BarPlot.png")
+
+
+
+
